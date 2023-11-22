19c679d20c8bce24213d25625727f2602c46abf1
GaneshBonde <ganeshbonde@gmail.com>
2023-11-22 23:51:39 +0530

    Oauth authorization server
    Created Oauth authorization server.

---
+++ src/main/java/com/demo/Oauthauthorizationserver/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.demo.Oauthauthorizationserver.repository;
+
+
+
+
+import com.demo.Oauthauthorizationserver.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
